be7f015e3bfcc5a1185f4402de05fbe15e3b49ad
Apurbo <tanvirmahfuz22@gmail.com>
2025-05-26 02:02:49 +0600

    adding vercel

--- backend/app/middlewares/multer.middleware.js
+++ backend/app/middlewares/multer.middleware.js
@@ -1,41 +1,42 @@
-import multer from "multer";
-import path from "path";
-import fs from "fs";
+// import multer from "multer";
+// import path from "path";
+// import fs from "fs";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-let uploadDir = path.resolve(__dirname, "../public/uploads");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, {recursive: true});
-}
-let fileName = null;
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    fileName = Date.now() + "-" + file.originalname;
-    cb(null, fileName);
-  },
-});
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
+// let uploadDir = path.resolve(__dirname, "../public/uploads");
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir, {recursive: true});
+// }
+// let fileName = null;
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, uploadDir);
+//   },
+//   filename: (req, file, cb) => {
+//     fileName = Date.now() + "-" + file.originalname;
+//     cb(null, fileName);
+//   },
+// });
 
-const upload = multer({
-  storage: storage,
-  limits: {fileSize: 10 * 1024 * 1024},
-  fileFilter: (req, file, cb) => {
-    console.log("multer invoked",file);
-    console.log(file.originalname);
-    uploadDir = uploadDir.replace("%20", " ");
-    console.log(uploadDir, "-----", file.originalname);
-    const filetypes = /jpeg|jpg|png/;
-    const mimetype = filetypes.test(file.mimetype);
-    const extname = filetypes.test(file.originalname.toLowerCase());
+// const upload = multer({
+//   storage: storage,
+//   limits: {fileSize: 10 * 1024 * 1024},
+//   fileFilter: (req, file, cb) => {
+//     console.log("multer invoked",file);
+//     console.log(file.originalname);
+//     uploadDir = uploadDir.replace("%20", " ");
+//     console.log(uploadDir, "-----", file.originalname);
+//     const filetypes = /jpeg|jpg|png/;
+//     const mimetype = filetypes.test(file.mimetype);
+//     const extname = filetypes.test(file.originalname.toLowerCase());
 
-    if (mimetype && extname) {
-      return cb(null, true);
-    } else {
-      cb("Error: Only images are allowed!");
-    }
-  },
-});
+//     if (mimetype && extname) {
+//       return cb(null, true);
+//     } else {
+//       cb("Error: Only images are allowed!");
+//     }
+//   },
+// });
+const upload = ()=>{return null}
 
 export default upload;
--- backend/app/routers/postRouter.js
+++ backend/app/routers/postRouter.js
@@ -2,14 +2,12 @@ import Router from "express";
 const postRouter = Router();
 import {isLoggedIn} from "../middlewares/auth.middleware.js";
 import * as postController from "../controllers/postController.js";
-import upload from "../middlewares/multer.middleware.js";
 
 postRouter.get("/", postController.getAllPostsController);
 postRouter.get("/post/:id", postController.getPostByIdController);
 postRouter.post(
   "/createPost",
   isLoggedIn,
-  upload.array("images", 5),
   postController.createPostController
 );
 postRouter.put("/updatePost", isLoggedIn, postController.updatePostController);

7380c5d35ffcd7cd10d7576e3e255922e6cfd075
Apurbo <tanvirmahfuz22@gmail.com>
2025-05-26 05:51:19 +0600

    trying proxy1 and updating socket origin

--- backend/app/utility/sockets.js
+++ backend/app/utility/sockets.js
@@ -5,7 +5,8 @@ import app from "../app.js";
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
+    credentials: true,
   },
 });
 
